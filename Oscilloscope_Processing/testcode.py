10dc3b304f2f46045dadb64b128cf8fd3a45b378
SriMukunthraj <53200048+DaBoss7b8@users.noreply.github.com>
2025-03-09 23:34:14 -0400

    Just some dingus reorganization while I'm on plane
    Just moved around some of the files, and trying to get the code to run while I finish the paper. Things are literally in the air, and I am things

---
+++ Oscilloscope_Processing/testcode.py
@@ -0,0 +1,20 @@
+import scipy.io
+import matplotlib.pyplot as plt
+mat = scipy.io.loadmat('file.mat')
+#print(mat)
+
+time = mat["time"]
+data = mat["data"]
+sample_rate = 1/mat["sampleInterval"][0]
+print(sample_rate)
+#print(data)
+
+plt.plot(data)
+plt.show()
+
+
+
+
+
+
+
